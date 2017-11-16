99418cdaa6c0ff5e083bad335d6d44eac263af1c
Willie Shen <williesh@usc.edu>
2017-11-15 22:08:35 -0800

    Adding view users

---
+++ src/GUI/ViewStudentsInClass.java
@@ -0,0 +1,69 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JButton;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+
+public class ViewStudentsInClass extends JFrame {
+
+	private JPanel contentPane;
+	private final Action action = new SwingAction();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ViewStudentsInClass frame = new ViewStudentsInClass();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ViewStudentsInClass() {
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblStudents = new JLabel("Students");
+		lblStudents.setBounds(70, 26, 92, 16);
+		contentPane.add(lblStudents);
+		
+		JList list = new JList();
+		list.setBounds(70, 69, 289, 163);
+		contentPane.add(list);
+		
+		JButton btnClose = new JButton("Close");
+		btnClose.setAction(action);
+		btnClose.setBounds(141, 244, 117, 29);
+		contentPane.add(btnClose);
+	}
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "Close");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+			setVisible(false);
+		}
+	}
+}
