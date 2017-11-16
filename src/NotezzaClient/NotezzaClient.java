6c60a4d9d2eb69263565d355af0e5b3c486be153
Tianyu Hu <tianyuh@usc.edu>
2017-11-15 18:22:51 -0800

    add oos and ois

--- src/NotezzaClient/NotezzaClient.java
+++ src/NotezzaClient/NotezzaClient.java
@@ -5,20 +5,33 @@ import java.net.Socket;
 
 public class NotezzaClient extends Thread {
 
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
+    
     NotezzaClient(String hostname, int port) {
         try {
-            System.out.println("Trying to connect to " + hostname + ":" + port);
+            System.out.println("Trying to establis connection to Notezza server at " + hostname + ":" + port);
             Socket s = new Socket(hostname, port);
-            System.out.println("Connected to " + hostname + ":" + port);
-
-
+            System.out.println("Connected to Notezza server at " + hostname + ":" + port);
+            ois = new ObjectInputStream(s.getInputStream());
+            oos = new ObjectOutputStream(s.getOutputStream());
             this.start();
+//            Wait for figuring out what to do here
+//            while(true) {
+//                
+//            }
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void run() {
+        while(true) {
+            Command cmd = (Command) ois.readObject();
+            Object obj = cmd.getObject();
+            
+        }
 
     }
 
