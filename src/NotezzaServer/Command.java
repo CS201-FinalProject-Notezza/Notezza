adb48beaacecb722b5ae5497527ed70a8df2978f
Tianyu Hu <tianyuh@usc.edu>
2017-11-15 17:56:50 -0800

    change Command class

--- src/NotezzaServer/Command.java
+++ src/NotezzaServer/Command.java
@@ -6,30 +6,19 @@ public class Command implements Serializable {
     public static final long serialVersionUID = 1;
 
     private CommandType type;
-    private String message;
-    private int number;
-    private boolean decision;
-
-    public Command(CommandType type, String message, int number, boolean decision) {
+    private Object obj;
+    
+    public Command(CommandType type, Object obj) {
         this.type = type;
-        this.message = message;
-        this.number = number;
-        this.decision = decision;
+        this.obj = obj;
     }
-
+    
     public CommandType getType() { return type; }
 
     public void setType(CommandType type) { this.type = type; }
 
-    public String getMessage() { return message; }
+    public Object getObject() { return obj; }
 
-    public void setMessage(String message) { this.message = message; }
+    public void setObject(Object obj) { this.obj = obj; }
 
-    public int getNumber() { return number;}
-
-    public void setNumber(int number) { this.number = number; }
-
-    public boolean isDecision() { return decision; }
-
-    public void setDecision(boolean decision) { this.decision = decision; }
 }
