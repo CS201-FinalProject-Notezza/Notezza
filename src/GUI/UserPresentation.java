4ff6863f00ff62ccda59a55f1b9c7be9281dd8fe
Joey Blundell <jmbiii@Joeys-MacBook-Pro.local>
2017-11-16 17:10:26 -0800

    Pretty-fied user presentation window

--- src/GUI/UserPresentation.java
+++ src/GUI/UserPresentation.java
@@ -1,21 +1,30 @@
 package GUI;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.border.BevelBorder;
 import javax.swing.border.EmptyBorder;
 
 public class UserPresentation extends JFrame {
 
 	private JPanel contentPane;
-	//private JPanel contentPane;
-	private JTextField textField;
+	private JTextField chatTextBox;
 
 	/**
 	 * Launch the application.
@@ -37,35 +46,151 @@ public class UserPresentation extends JFrame {
 	 * Create the frame.
 	 */
 	public UserPresentation() {
+		setTitle("Presentation");
+		setResizable(false);
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+		} catch (Exception e) {
+		    // If Nimbus is not available, you can set the GUI to another look and feel.
+		}
 		setBounds(100, 100, 791, 545);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JList list = new JList();
-		list.setBounds(500, 258, 265, 189);
-		contentPane.add(list);
+		JList chatWindow = new JList();
+		chatWindow.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		chatWindow.setBounds(500, 271, 272, 202);
+		contentPane.add(chatWindow);
 		
-		textField = new JTextField();
-		textField.setBounds(574, 449, 164, 27);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		chatTextBox = new JTextField();
+		chatTextBox.setBounds(499, 473, 207, 27);
+		contentPane.add(chatTextBox);
+		chatTextBox.setColumns(10);
 		
-		JButton btnSendMessage = new JButton("Send Message");
-		btnSendMessage.setBounds(574, 488, 149, 29);
-		contentPane.add(btnSendMessage);
+		JButton sendChatButton = new JButton("Send");
+		sendChatButton.setBounds(707, 473, 68, 29);
+		contentPane.add(sendChatButton);
 		
-		JButton button = new JButton("<");
-		button.setBounds(140, 469, 38, 29);
-		contentPane.add(button);
+		JPanel slidePanel = new JPanel();
+		slidePanel.setBorder(null);
+		slidePanel.setBounds(20, 37, 468, 409);
+		contentPane.add(slidePanel);
 		
-		JButton button_1 = new JButton(">");
-		button_1.setBounds(222, 469, 38, 29);
-		contentPane.add(button_1);
+		try {
+			URL temp = new URL("http://1.bp.blogspot.com/-Uuu510AUdjk/Vqqo0jAUe5I/AAAAAAAAAc8/UCdgGmH5EUc/s1600/figure_01.gif");
+			ImageIcon slideImage = new ImageIcon(temp);
+			JLabel slideImageLabel = new JLabel("");
+			slideImageLabel.setIcon(slideImage);
+			slidePanel.add(slideImageLabel);
+		} catch (MalformedURLException murle) { }
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(32, 42, 427, 383);
-		contentPane.add(panel);
+		JPanel quizPanel = new JPanel();
+		quizPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		quizPanel.setBounds(500, 42, 272, 218);
+		contentPane.add(quizPanel);
+		GridBagLayout gbl_quizPanel = new GridBagLayout();
+		gbl_quizPanel.columnWidths = new int[] {136, 133, 136};
+		gbl_quizPanel.rowHeights = new int[] {50, 28, 28, 28, 28, 28, 28};
+		gbl_quizPanel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gbl_quizPanel.rowWeights = new double[]{Double.MIN_VALUE, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+		quizPanel.setLayout(gbl_quizPanel);
+		
+		JLabel questionLabel = new JLabel("Quiz Question");
+		questionLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		GridBagConstraints gbc_questionLabel = new GridBagConstraints();
+		gbc_questionLabel.anchor = GridBagConstraints.WEST;
+		gbc_questionLabel.gridwidth = 2;
+		gbc_questionLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_questionLabel.gridx = 0;
+		gbc_questionLabel.gridy = 0;
+		quizPanel.add(questionLabel, gbc_questionLabel);
+		
+		JCheckBox answer1 = new JCheckBox("Answer 1");
+		GridBagConstraints gbc_answer1 = new GridBagConstraints();
+		gbc_answer1.anchor = GridBagConstraints.WEST;
+		gbc_answer1.gridwidth = 2;
+		gbc_answer1.insets = new Insets(0, 0, 5, 5);
+		gbc_answer1.gridx = 0;
+		gbc_answer1.gridy = 1;
+		quizPanel.add(answer1, gbc_answer1);
+		
+		JCheckBox answer2 = new JCheckBox("Answer 2");
+		GridBagConstraints gbc_answer2 = new GridBagConstraints();
+		gbc_answer2.anchor = GridBagConstraints.WEST;
+		gbc_answer2.gridwidth = 2;
+		gbc_answer2.insets = new Insets(0, 0, 5, 5);
+		gbc_answer2.gridx = 0;
+		gbc_answer2.gridy = 2;
+		quizPanel.add(answer2, gbc_answer2);
+		
+		JCheckBox answer3 = new JCheckBox("Answer 3");
+		GridBagConstraints gbc_answer3 = new GridBagConstraints();
+		gbc_answer3.anchor = GridBagConstraints.WEST;
+		gbc_answer3.gridwidth = 2;
+		gbc_answer3.insets = new Insets(0, 0, 5, 5);
+		gbc_answer3.gridx = 0;
+		gbc_answer3.gridy = 3;
+		quizPanel.add(answer3, gbc_answer3);
+		
+		JCheckBox answer4 = new JCheckBox("Answer 4");
+		GridBagConstraints gbc_answer4 = new GridBagConstraints();
+		gbc_answer4.anchor = GridBagConstraints.WEST;
+		gbc_answer4.gridwidth = 2;
+		gbc_answer4.insets = new Insets(0, 0, 5, 5);
+		gbc_answer4.gridx = 0;
+		gbc_answer4.gridy = 4;
+		quizPanel.add(answer4, gbc_answer4);
+		
+		JCheckBox answer5 = new JCheckBox("Answer 5");
+		GridBagConstraints gbc_answer5 = new GridBagConstraints();
+		gbc_answer5.anchor = GridBagConstraints.WEST;
+		gbc_answer5.gridwidth = 2;
+		gbc_answer5.insets = new Insets(0, 0, 5, 5);
+		gbc_answer5.gridx = 0;
+		gbc_answer5.gridy = 5;
+		quizPanel.add(answer5, gbc_answer5);
+		
+		JButton checkAnswerButton = new JButton("Check");
+		GridBagConstraints gbc_checkAnswerButton = new GridBagConstraints();
+		gbc_checkAnswerButton.insets = new Insets(0, 0, 0, 5);
+		gbc_checkAnswerButton.gridx = 0;
+		gbc_checkAnswerButton.gridy = 6;
+		quizPanel.add(checkAnswerButton, gbc_checkAnswerButton);
+		
+		JLabel correctLabel = new JLabel("Correct/Incorrect");
+		GridBagConstraints gbc_correctLabel = new GridBagConstraints();
+		gbc_correctLabel.gridx = 1;
+		gbc_correctLabel.gridy = 6;
+		quizPanel.add(correctLabel, gbc_correctLabel);
+		
+		JPanel slideButtonPanel = new JPanel();
+		slideButtonPanel.setBounds(17, 473, 468, 27);
+		contentPane.add(slideButtonPanel);
+		GridBagLayout gbl_slideButtonPanel = new GridBagLayout();
+		gbl_slideButtonPanel.columnWidths = new int[] {220, 28, 220};
+		gbl_slideButtonPanel.rowHeights = new int[]{0, 0};
+		gbl_slideButtonPanel.columnWeights = new double[]{0.0, 0.0, 0.0};
+		gbl_slideButtonPanel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		slideButtonPanel.setLayout(gbl_slideButtonPanel);
+		
+		JButton slideBackwardsButton = new JButton("<");
+		GridBagConstraints gbc_slideBackwardsButton = new GridBagConstraints();
+		gbc_slideBackwardsButton.insets = new Insets(0, 0, 0, 5);
+		gbc_slideBackwardsButton.gridx = 0;
+		gbc_slideBackwardsButton.gridy = 0;
+		slideButtonPanel.add(slideBackwardsButton, gbc_slideBackwardsButton);
+		
+		JButton slideForwardButton = new JButton(">");
+		GridBagConstraints gbc_slideForwardButton = new GridBagConstraints();
+		gbc_slideForwardButton.gridx = 2;
+		gbc_slideForwardButton.gridy = 0;
+		slideButtonPanel.add(slideForwardButton, gbc_slideForwardButton);
 	}
 }
