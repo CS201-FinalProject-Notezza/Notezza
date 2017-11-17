6f5c1efc3e9d01fb99e21bed9690c23b85e667ce
zifanshi <zifanshi@usc.edu>
2017-11-16 19:45:24 -0800

    add absolute jar for netbeans

--- src/NewGUI/NotezzaGUI.java
+++ src/NewGUI/NotezzaGUI.java
@@ -3,11 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package notezzagui;
+package NewGUI;
 
 import java.awt.BorderLayout;
 import javax.swing.*;
-import org.netbeans.lib.awtextra.AbsoluteLayout;
 
 
 /**
