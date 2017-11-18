f7f909355f63a1d0c9f84d326698dc4311af2179
Joey Blundell <jmbiii@Joeys-MacBook-Pro.local>
2017-11-18 14:43:57 -0800

    Pretty UserProfile

--- src/GUI/UserProfile.java
+++ src/GUI/UserProfile.java
@@ -13,6 +13,10 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
 
 public class UserProfile extends JFrame {
 
@@ -40,20 +44,10 @@ public class UserProfile extends JFrame {
 	 * Create the frame.
 	 */
 	public UserProfile(User user) {
+		setResizable(false);
 		this.user = user;
-
-		try {
-			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-				if ("Nimbus".equals(info.getName())) {
-					UIManager.setLookAndFeel(info.getClassName());
-					break;
-				}
-			}
-		} catch (Exception e) {
-			// If Nimbus is not available, you can set the GUI to another look and feel.
-		}
 		
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 320, 162);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -67,39 +61,62 @@ public class UserProfile extends JFrame {
 			lName = user.getLname();
 			email = user.getEmail();
 		} else {
-			userName = "N/A";
-			fName = "N/A";
-			lName = "N/A";
-			email = "N/A";
+			userName = user.getUsername();
+			fName = "";
+			lName = "";
+			email = "";
 		}
+		
+		JLabel privateLabel = new JLabel("This user's profile is private!");
+		privateLabel.setForeground(new Color(178, 34, 34));
+		privateLabel.setFont(new Font("Dialog", Font.BOLD | Font.ITALIC, 16));
+		privateLabel.setEnabled(false);
+		privateLabel.setBounds(37, 65, 245, 26);
+		contentPane.add(privateLabel);
+		
+		JLabel lnameText = new JLabel("<dynamic>");
+		lnameText.setHorizontalAlignment(SwingConstants.RIGHT);
+		lnameText.setBounds(116, 69, 184, 26);
+		contentPane.add(lnameText);
 
-		JLabel lblUsername = new JLabel("Username: " + userName);
-		lblUsername.setBounds(100, 75, 310, 16);
-		contentPane.add(lblUsername);
+		JLabel usernameLabel = new JLabel("Username:");
+		usernameLabel.setBounds(20, 20, 100, 16);
+		contentPane.add(usernameLabel);
 
-		JLabel lblFirstName = new JLabel("First Name: " + fName);
-		lblFirstName.setBounds(100, 93, 284, 16);
-		contentPane.add(lblFirstName);
+		JLabel fnameLabel = new JLabel("First Name:");
+		fnameLabel.setBounds(20, 47, 100, 16);
+		contentPane.add(fnameLabel);
 		
-		JLabel lblLastName = new JLabel("Last Name: " + lName);
-		lblLastName.setBounds(100, 111, 268, 16);
-		contentPane.add(lblLastName);
+		JLabel lnameLabel = new JLabel("Last Name:");
+		lnameLabel.setBounds(20, 74, 100, 16);
+		contentPane.add(lnameLabel);
 		
-		JCheckBox chckbxPrivate = new JCheckBox("Private");
-		chckbxPrivate.setBounds(147, 177, 128, 23);
-		contentPane.add(chckbxPrivate);
+		JLabel emailLabel = new JLabel("Email:");
+		emailLabel.setBounds(20, 101, 100, 16);
+		contentPane.add(emailLabel);
 		
-		JLabel lblEmail = new JLabel("Email: " + email);
-		lblEmail.setBounds(100, 131, 268, 16);
-		contentPane.add(lblEmail);
+		JLabel fnameText = new JLabel("<dynamic>");
+		fnameText.setHorizontalAlignment(SwingConstants.RIGHT);
+		fnameText.setBounds(116, 42, 184, 26);
+		contentPane.add(fnameText);
 		
-		JButton btnApply = new JButton("Apply");
-		btnApply.setBounds(68, 212, 117, 29);
-		contentPane.add(btnApply);
+		JLabel emailText = new JLabel("<dynamic>");
+		emailText.setHorizontalAlignment(SwingConstants.RIGHT);
+		emailText.setBounds(116, 96, 184, 26);
+		contentPane.add(emailText);
 		
-		JButton btnCancel = new JButton("Cancel");
-		btnCancel.setBounds(214, 212, 117, 29);
-		contentPane.add(btnCancel);
+		JLabel usernameText = new JLabel("<dynamic>");
+		usernameText.setHorizontalAlignment(SwingConstants.RIGHT);
+		usernameText.setBounds(116, 15, 184, 26);
+		contentPane.add(usernameText);
+		
+		if (!user.isVisible()) {
+			privateLabel.setEnabled(true);
+			fnameLabel.setEnabled(false);
+			fnameText.setEnabled(false);
+			lnameLabel.setEnabled(false);
+			lnameText.setEnabled(false);
+		}
 	}
 
 }
