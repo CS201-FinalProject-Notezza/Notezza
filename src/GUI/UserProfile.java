a534f8f6ec2c27636c390124af672196db3bc53d
Joey Blundell <jmbiii@Joeys-MacBook-Pro.local>
2017-11-18 14:52:39 -0800

    Fixed hover over issue in login and finished prettifying UserProfile

--- src/GUI/UserProfile.java
+++ src/GUI/UserProfile.java
@@ -55,67 +55,76 @@ public class UserProfile extends JFrame {
 
 		String userName,fName,lName,email;
 
-		if (user.isVisible()) {
-			userName = user.getUsername();
-			fName = user.getFname();
-			lName = user.getLname();
-			email = user.getEmail();
-		} else {
-			userName = user.getUsername();
-			fName = "";
-			lName = "";
-			email = "";
-		}
+		userName = user.getUsername();
+		fName = user.getFname();
+		lName = user.getLname();
+		email = user.getEmail();
 		
 		JLabel privateLabel = new JLabel("This user's profile is private!");
 		privateLabel.setForeground(new Color(178, 34, 34));
 		privateLabel.setFont(new Font("Dialog", Font.BOLD | Font.ITALIC, 16));
-		privateLabel.setEnabled(false);
+		privateLabel.setVisible(false);
 		privateLabel.setBounds(37, 65, 245, 26);
 		contentPane.add(privateLabel);
 		
-		JLabel lnameText = new JLabel("<dynamic>");
+		JLabel lnameText = new JLabel(lName);
 		lnameText.setHorizontalAlignment(SwingConstants.RIGHT);
 		lnameText.setBounds(116, 69, 184, 26);
+		lnameText.setForeground(new java.awt.Color(204, 204, 204));
+		lnameText.setBorder(null);
 		contentPane.add(lnameText);
 
 		JLabel usernameLabel = new JLabel("Username:");
 		usernameLabel.setBounds(20, 20, 100, 16);
+		usernameLabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		usernameLabel.setForeground(new java.awt.Color(204, 204, 204));
 		contentPane.add(usernameLabel);
 
 		JLabel fnameLabel = new JLabel("First Name:");
 		fnameLabel.setBounds(20, 47, 100, 16);
+		fnameLabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		fnameLabel.setForeground(new java.awt.Color(204, 204, 204));
 		contentPane.add(fnameLabel);
 		
 		JLabel lnameLabel = new JLabel("Last Name:");
 		lnameLabel.setBounds(20, 74, 100, 16);
+		lnameLabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		lnameLabel.setForeground(new java.awt.Color(204, 204, 204));
 		contentPane.add(lnameLabel);
 		
 		JLabel emailLabel = new JLabel("Email:");
 		emailLabel.setBounds(20, 101, 100, 16);
+		emailLabel.setFont(new Font("Dialog", Font.BOLD, 15));
+		emailLabel.setForeground(new java.awt.Color(204, 204, 204));
 		contentPane.add(emailLabel);
 		
-		JLabel fnameText = new JLabel("<dynamic>");
+		JLabel fnameText = new JLabel(fName);
 		fnameText.setHorizontalAlignment(SwingConstants.RIGHT);
 		fnameText.setBounds(116, 42, 184, 26);
+		fnameText.setForeground(new java.awt.Color(204, 204, 204));
+		fnameText.setBorder(null);
 		contentPane.add(fnameText);
 		
-		JLabel emailText = new JLabel("<dynamic>");
+		JLabel emailText = new JLabel(email);
 		emailText.setHorizontalAlignment(SwingConstants.RIGHT);
 		emailText.setBounds(116, 96, 184, 26);
+		emailText.setForeground(new java.awt.Color(204, 204, 204));
+		emailText.setBorder(null);
 		contentPane.add(emailText);
 		
-		JLabel usernameText = new JLabel("<dynamic>");
+		JLabel usernameText = new JLabel(userName);
 		usernameText.setHorizontalAlignment(SwingConstants.RIGHT);
 		usernameText.setBounds(116, 15, 184, 26);
+		usernameText.setForeground(new java.awt.Color(204, 204, 204));
+		usernameText.setBorder(null);
 		contentPane.add(usernameText);
 		
 		if (!user.isVisible()) {
-			privateLabel.setEnabled(true);
-			fnameLabel.setEnabled(false);
-			fnameText.setEnabled(false);
-			lnameLabel.setEnabled(false);
-			lnameText.setEnabled(false);
+			privateLabel.setVisible(true);
+			fnameLabel.setVisible(false);
+			fnameText.setVisible(false);
+			lnameLabel.setVisible(false);
+			lnameText.setVisible(false);
 		}
 	}
 
